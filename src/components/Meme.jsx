b0c7bee753c240e90f909f10bef209047511519d
Fideltodayy <fidelotieno11@gmail.com>
2023-11-26 16:52:23 +0300

    Meme can be successfully downloaded

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import memesData from "../../memesDAta"
-
+import {saveAs} from "file-saver"
+import { useRef } from "react"
 export default function () {
     const [meme, setMeme] = React.useState({
         topText: "",
@@ -9,6 +10,7 @@ export default function () {
     })
 
     const [allMemeImages,setAllMemeImages] = React.useState(memesData)
+    const memeContainerRef = useRef(null);
 
     function getMemeImage() {
         const memesArray = allMemeImages.data.memes
@@ -21,6 +23,44 @@ export default function () {
         const {name, value} = event.target
         setMeme(prevMeme => ({...prevMeme, [name]: value}))
     }
+    
+    const handleDownload = () => {
+        const memeContainer = memeContainerRef.current;
+    
+        const canvas = document.createElement('canvas');
+        canvas.width = memeContainer.offsetWidth;
+        canvas.height = memeContainer.offsetHeight;
+    
+        const context = canvas.getContext('2d');
+        const image = new Image();
+        image.crossOrigin = 'anonymous'; // Enable CORS for the image
+    
+        image.onload = () => {
+            // Draw the image onto the canvas
+            context.drawImage(image, 0, 0, canvas.width, canvas.height);
+    
+            // Set font properties
+            context.font = 'bold 30px "impact", cursive'; // Adjust size as needed
+            context.fillStyle = 'white'; // Adjust text color as needed
+            context.textAlign = 'center';
+    
+            // Draw the top text in the center
+            context.fillText(meme.topText, canvas.width / 2, 40);
+    
+            // Draw the bottom text in the center
+            context.fillText(meme.bottomText, canvas.width / 2, canvas.height - 20);
+    
+            // Get the canvas data as a data URL
+            const dataUrl = canvas.toDataURL('image/png');
+    
+            // Use file-saver to trigger the download
+            saveAs(dataUrl, 'meme.png');
+        };
+    
+        // Set the source of the image to the URL from your state
+        image.src = meme.randomImage;
+    };
+    
     
     return(
         <main className=" p-4">
@@ -43,16 +83,16 @@ export default function () {
                 />
                 <button className=" mt-2 grid-cols-1 border-2 border-black rounded-lg bg-blue-950 text-white cursor-pointer w-full font-extrabold text-xl" onClick={getMemeImage}>Generate</button>
             </div>
-            <div className="meme">
+            <div className="meme" ref={memeContainerRef}>
                 <img src={meme.randomImage} className="meme--image" />
                 <div className=" h-8 absolute bottom-0 right-0">
                     <p className="text-white text-center text-sm">@Fideltodayy</p>
                 </div>
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
-                <a className="mt-2 grid-cols-1 border-2 border-black rounded-lg bg-blue-950 text-white cursor-pointer w-full font-extrabold text-xl " href="/saved.jpg" download="Saved.jpg" >Save Image</a>
 
             </div>
+            <button className="mt-2 grid-cols-1 border-2 border-black rounded-lg bg-blue-950 text-white cursor-pointer w-full font-extrabold text-xl"onClick={handleDownload}>Save Image</button>
 
         </main>
     )
